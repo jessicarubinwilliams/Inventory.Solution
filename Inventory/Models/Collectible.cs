c6a2d22d1d87e15988f97999cfa5821bb0f0de0c
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-10-03 14:50:44 -0400

    Fix property name errors
    Co-authored-by: Sisi Vieira <cicy886@gmail.com>
    Co-authored-by: Alex Goldberg <alexdominguezwebdev@gmail.com>

--- Inventory/Models/Collectible.cs
+++ Inventory/Models/Collectible.cs
@@ -3,7 +3,7 @@ namespace Inventory.Models
   public class Collectible
   {
     public int CollectibleId { get; set; }
-    public string Description { get; set; }
+    public string Name { get; set; }
     public int Quantity { get; set; }
   }
 }
