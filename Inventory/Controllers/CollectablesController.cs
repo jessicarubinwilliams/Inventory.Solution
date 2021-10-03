1e8c6eb752a04399b5455852d1b0f3e6040133a5
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-10-03 17:58:52 -0400

    Add functionality for editing a collectible
    Co-authored-by: Sisi Vieira <cicy886@gmail.com>
    Co-authored-by: Alex Goldberg <alexdominguezwebdev@gmail.com>

--- Inventory/Controllers/CollectablesController.cs
+++ Inventory/Controllers/CollectablesController.cs
@@ -1,5 +1,6 @@
 using Inventory.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -22,21 +23,35 @@ namespace Inventory.Controllers
 
     public ActionResult Create()
     {
-        return View();
+      return View();
     }
 
     [HttpPost]
     public ActionResult Create(Collectible collectible)
     {
-        _db.Collectables.Add(collectible);
-        _db.SaveChanges();
-        return RedirectToAction("Index");
+      _db.Collectables.Add(collectible);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
 
     public ActionResult Details(int id)
     {
-        Collectible thisCollectible = _db.Collectables.FirstOrDefault(collectible => collectible.CollectibleId == id);
-        return View(thisCollectible);
+      Collectible thisCollectible = _db.Collectables.FirstOrDefault(collectible => collectible.CollectibleId == id);
+      return View(thisCollectible);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisCollectible = _db.Collectables.FirstOrDefault(collectible => collectible.CollectibleId == id);
+      return View(thisCollectible);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Collectible collectible)
+    {
+      _db.Entry(collectible).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
