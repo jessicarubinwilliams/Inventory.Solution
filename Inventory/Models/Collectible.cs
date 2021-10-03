d168ae2e4e51bed5b8904f4400784a821a5fd4f3
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-10-03 19:14:04 -0400

    Update Category class for its new relationship with Collection class
    Co-authored-by: Sisi Vieira <cicy886@gmail.com>
    Co-authored-by: Alex Goldberg <alexdominguezwebdev@gmail.com>

--- Inventory/Models/Collectible.cs
+++ Inventory/Models/Collectible.cs
@@ -5,5 +5,8 @@ namespace Inventory.Models
     public int CollectibleId { get; set; }
     public string Name { get; set; }
     public int Quantity { get; set; }
+
+    public int CollectionId { get; set; }
+    public virtual Collection Collection { get; set; }
   }
 }
