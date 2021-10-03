c744e3d77ab9f779226c42662278088e6322d4c1
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-10-03 18:45:50 -0400

    Add access to collections table in database
    Co-authored-by: Sisi Vieira <cicy886@gmail.com>
    Co-authored-by: Alex Goldberg <alexdominguezwebdev@gmail.com>

--- Inventory/Models/InventoryContext.cs
+++ Inventory/Models/InventoryContext.cs
@@ -4,8 +4,14 @@ namespace Inventory.Models
 {
   public class InventoryContext : DbContext
   {
+    public DbSet<Collection> Collections { get; set; }
     public DbSet<Collectible> Collectables { get; set; }
+   
 
     public InventoryContext(DbContextOptions options) : base(options) { }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
   }
 }
