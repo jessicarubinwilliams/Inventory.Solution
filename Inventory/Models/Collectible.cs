26e4e268cc851b7b9707d954760f62cbff70d466
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-10-03 14:16:37 -0400

    Add Collectible class & its auto-implemented properties
    Co-authored-by: Sisi Vieira <cicy886@gmail.com>
    Co-authored-by: Alex Goldberg <alexdominguezwebdev@gmail.com>

---
+++ Inventory/Models/Collectible.cs
@@ -0,0 +1,9 @@
+namespace Inventory.Models
+{
+  public class Collectible
+  {
+    public int CollectibleId { get; set; }
+    public string Description { get; set; }
+    public int Quantity { get; set; }
+  }
+}
